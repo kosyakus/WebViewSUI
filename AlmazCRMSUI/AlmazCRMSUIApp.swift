0c963bd2dbd11f5ef80da5b0fade9e81a93878a1
Natali <ns@numbuster.com>
2025-02-01 12:46:11 +0300

    Initial Commit

---
+++ AlmazCRMSUI/AlmazCRMSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlmazCRMSUIApp.swift
+//  AlmazCRMSUI
+//
+//  Created by Natalia Sinitsyna on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AlmazCRMSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlmazCRMSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlmazCRMSUI
+//
+//  Created by Natalia Sinitsyna on 01.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
