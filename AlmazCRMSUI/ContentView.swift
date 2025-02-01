92791b48be5ce47e1fb750b8d725988e45ca5223
Natali <ns@numbuster.com>
2025-02-01 12:58:08 +0300

    Initial commit

--- AlmazCRMSUI/ContentView.swift
+++ AlmazCRMSUI/ContentView.swift
@@ -6,16 +6,30 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        // WebView — это кастомная структура, которую мы создадим ниже
+        WebView(url: URL(string: "https://habr.com")!)
+//            .edgesIgnoringSafeArea(.all) // Чтобы WebView занял весь экран
+    }
+}
+
+// WebView, построенный на UIViewRepresentable, чтобы встроить WKWebView в SwiftUI
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        // Создаём экземпляр WKWebView
+        let webView = WKWebView()
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // Загружаем сайт по URL
+        let request = URLRequest(url: url)
+        uiView.load(request)
     }
 }
 
